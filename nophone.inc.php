a1cb6655fe37769a7dcc7e932212d0553928e0c0
拥抱脸p <19573111046@139.com>
2024-04-19 17:22:20 +0800

    a

--- nophone.inc.php
+++ nophone.inc.php
@@ -1,11 +1,7 @@
-<?php $fucktips = str_replace(array("<",">","&"," "),array("&lt;","&gt;","&amp;","&nbsp;"),$_G['cache']['plugin']['phone_user_and_dog_forbidden']["fucktips"]);?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8"/>
-        <title><?php echo $fucktips;?></title>
-    </head>
-    <body style="width:600px;">
-         <h1 style="color:red"><?php echo $fucktips;?></h1>
-    </body>
-</html>
+<?php
+if(!defined('IN_DISCUZ')) {
+	exit('黑客S全家');
+}
+$fucktips = str_replace(array("<",">","&"," "),array("&lt;","&gt;","&amp;","&nbsp;"),$_G['cache']['plugin']['phone_user_and_dog_forbidden']["fucktips"];
+include template('arclight_no_phone_login:no_phone');
+?>
